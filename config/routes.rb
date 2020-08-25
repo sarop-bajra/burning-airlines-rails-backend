4655a4e8d66004853633340e874d2f1e5794aa0b
Jamil Samarani <jamilsamarani@hotmail.com>
2020-08-26 09:58:51 +1000

    fixed typos

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'planes/new'
-  get 'planes/create'
-  get 'planes/index'
-  get 'planes/show'
-  get 'planes/edit'
-  get 'planes/update'
-  get 'planes/destroy'
-  # Airplane
+  # Plane
   resources :airplanes
 
-  # Flihgt
+  # Flight
   resources :flights
 
-  # Test
 
 end
