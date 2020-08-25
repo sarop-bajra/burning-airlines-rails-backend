4918d68ef5863a434c6b7e41e97935b8e5431374
Ryan Xin <xinpf0715@gmail.com>
2020-08-25 20:42:27 +1000

    airplane & flight controller created

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,22 @@
+class FlightsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
