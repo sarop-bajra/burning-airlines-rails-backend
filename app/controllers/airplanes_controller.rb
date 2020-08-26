155daea16d70f233875d719e951c6b758296a55d
Swaroop Bajra <sarop_bajra@hotmail.com>
2020-08-26 10:14:36 +1000

    backend issue fixing

---
+++ app/controllers/airplanes_controller.rb
@@ -0,0 +1,28 @@
+class AirplanesController < ApplicationController
+
+  # Create Create
+  def new
+  end
+
+  def create
+  end
+
+  # Read
+  def index
+  end
+
+  def show
+  end
+
+  # Update
+  def edit
+  end
+
+  def update
+  end
+
+  # Delete
+  def destroy
+  end
+
+end
